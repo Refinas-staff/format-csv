095d52bcca570a50c35b93e17f3e2d39d10b3bc2
Refinas-staff <refinas.shuukei2@gmail.com>
2026-06-12 15:17:02 +0900

    Update patterns.js

--- patterns.js
+++ patterns.js
@@ -959,6 +959,7 @@
           label: "口座CSV",
           type: "file",
           required: true,
+          headerRow: 3,
           help: "口座名義人で照合します。委託者カナ氏名は取引銀行に使用します。",
           inputHeaders: [
             "委託者カナ氏名",
